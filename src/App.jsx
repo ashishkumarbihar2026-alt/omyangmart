753e0afdeceb331d11f9bd9501f731a42b0bc5ed
ashishkumarbihar2026-alt <ashishkumarbihar2026@gmail.com>
2026-06-24 15:09:29 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
+import Admin from './pages/Admin'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
