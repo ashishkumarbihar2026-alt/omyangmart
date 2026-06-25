18904be04c66d3cfc0d3289ba7f5600636c8e402
ashishkumarbihar2026-alt <ashishkumarbihar2026@gmail.com>
2026-06-25 16:15:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Cart from './pages/Cart'
 import Admin from './pages/Admin'
+import Profile from './pages/Profile'
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   )
